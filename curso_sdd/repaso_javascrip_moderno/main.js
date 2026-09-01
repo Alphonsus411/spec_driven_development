cb8e85d84ca695ed142aabd5cd63e638a94b82d8
Adolfo Gonzalez <adolfogonzal@gmail.com>
2026-09-01 14:04:07 +0200

    Añadimos ejemplos de JavaScript a la carpeta de muestra

--- curso_sdd/repaso_javascrip_moderno/main.js
+++ curso_sdd/repaso_javascrip_moderno/main.js
@@ -10,7 +10,9 @@ console.log(nombre);
 
 // Constantes
 const apellido = "Pérez"; // No se puede reasignar el valor de una constante
-const altura = 1.75;
+
+// Otra variable
+let altura = 1.75;
 
 // Mostrar el valor de la constante en la consola
 console.log(nombre);
@@ -27,3 +29,55 @@ datos.innerHTML = `
     <h1>Soy la caja de datos</h1>
     <h2>Mi nombre es: ${concatenacion}</h2>
 `;
+
+// Condicionales
+altura = 1.85;
+if (altura >= 1.85){
+    datos.innerHTML += "<h1>Eres una persona alta</h1>";
+} else {
+    datos.innerHTML += "<h1>Eres una persona baja</h1>";
+}
+
+// Bucles
+for(let year = 2000; year <= 2023; year++){
+   datos.innerHTML += `<h2>Estamos en el año: ${year}</h2>`; 
+}
+
+// Arrays
+let nombres = ["Juan", "Pedro", "María", "Ana"];
+
+let divNombres = document.querySelector("#nombres");
+
+divNombres.innerHTML = nombres[3]; // Muestra el primer elemento del array
+
+// Recorriendo un array con un bucle for
+divNombres.innerHTML = "<h1>Listado de nombres:</h1><ul>";
+
+/* nombres.forEach((nombre) => {
+    divNombres.innerHTML += `<li>${nombre}</li>`;
+});*/
+
+for (let nombre of nombres){
+    divNombres.innerHTML += `<li>${nombre}</li>`;
+}
+
+divNombres.innerHTML += "</ul>"
+
+// Funciones
+
+const miInformacion = (nombre, altura) => {
+    let misDatos = `
+        <h1>Mi nombre es: ${nombre}</h1>
+        <h2>Mido: ${altura} metros</h2>
+    `;
+    return misDatos;
+}
+
+console.log(miInformacion("Rodolfo", "1.85"));
+
+const imprimir = () => {
+    let datos = document.querySelector("#datos"); 
+    datos.innerHTML = miInformacion("Rodolfo", "1.85");
+}
+
+imprimir();
